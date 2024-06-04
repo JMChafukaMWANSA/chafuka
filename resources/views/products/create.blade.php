e2588a53520d4397bd52d27e1a1e2964b94888a4
Kiseki7 <81614691+Kiseki7@users.noreply.github.com>
2024-06-04 10:59:40 +0200

    first commit

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,71 @@
+@extends('products.layouts')
+
+@section('content')
+
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-8">
+
+            <div class="card">
+                <div class="card-header">
+                    <div class="float-start">
+                        Add New Product
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('products.store') }}" method="post">
+                        @csrf
+
+                        <div class="mb-3 row">
+                            <label for="code" class="col-md-4 col-form-label text-md-end text-start">product</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control @error('product') is-invalid @enderror" id="product" name="product" value="{{ old('product') }}">
+                                @if ($errors->has('product'))
+                                    <span class="text-danger">{{ $errors->first('product') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="quantity" class="col-md-4 col-form-label text-md-end text-start">Quantity</label>
+                            <div class="col-md-6">
+                                <input type="number" class="form-control @error('quantity') is-invalid @enderror" id="quantity" name="quantity" value="{{ old('quantity') }}">
+                                @if ($errors->has('quantity'))
+                                    <span class="text-danger">{{ $errors->first('quantity') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="price" class="col-md-4 col-form-label text-md-end text-start">Price</label>
+                            <div class="col-md-6">
+                                <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ old('price') }}">
+                                @if ($errors->has('price'))
+                                    <span class="text-danger">{{ $errors->first('price') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="description" class="col-md-4 col-form-label text-md-end text-start">Description</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ old('description') }}</textarea>
+                                @if ($errors->has('description'))
+                                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Add Product">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,78 @@
+@extends('products.layouts')
+
+@section('content')
+
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-8">
+
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ $message }}
+                </div>
+            @endif
+
+            <div class="card">
+                <div class="card-header">
+                    <div class="float-start">
+                        Edit Product
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{ route('products.update', $product->id) }}" method="post">
+                        @csrf
+                        @method("PUT")
+
+                        <div class="mb-3 row">
+                            <label for="code" class="col-md-4 col-form-label text-md-end text-start">product</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control @error('product') is-invalid @enderror" id="product" name="product" value="{{ $product->product }}">
+                                @if ($errors->has('product'))
+                                    <span class="text-danger">{{ $errors->first('product') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="quantity" class="col-md-4 col-form-label text-md-end text-start">Quantity</label>
+                            <div class="col-md-6">
+                                <input type="number" class="form-control @error('quantity') is-invalid @enderror" id="quantity" name="quantity" value="{{ $product->quantity }}">
+                                @if ($errors->has('quantity'))
+                                    <span class="text-danger">{{ $errors->first('quantity') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="price" class="col-md-4 col-form-label text-md-end text-start">Price</label>
+                            <div class="col-md-6">
+                                <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{ $product->price }}">
+                                @if ($errors->has('price'))
+                                    <span class="text-danger">{{ $errors->first('price') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="description" class="col-md-4 col-form-label text-md-end text-start">Description</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{ $product->description }}</textarea>
+                                @if ($errors->has('description'))
+                                    <span class="text-danger">{{ $errors->first('description') }}</span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <input type="submit" class="col-md-3 offset-md-5 btn btn-primary" value="Update">
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
